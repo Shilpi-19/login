7eba767058782fc0545db088a0b836e5ea71baf7
Shilpi-19 <shilpishukla.jp@gmail.com>
2024-08-08 18:27:45 +0530

    remove commented code

--- src/App.js
+++ src/App.js
@@ -1,19 +1,3 @@
-// import React from 'react';
-// import './App.css';
-// import Login from './Pages/Login/Login';
-// import Signup from './Pages/Signup/Signup';
- 
-// const App = () => {
-//   return (
-//     <>
-//     <Login />
-//     <Signup />
-//     </>
-//   )
-// }
- 
-// export default App;
- 
 import React from 'react';
 import { BrowserRouter, Routes ,Route} from 'react-router-dom';
 import Login from './Pages/Login/Login';
